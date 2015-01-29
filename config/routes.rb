4ceceb7d3070fd846491988c71746b6366b6c2f7
Paul Colusso <paulcolusso@gmail.com>
2015-01-29 22:10:14 +1100

    moar user stuffs

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-
   get 'signup' => 'users/new'
-
+  resources :users
+  root 'welcome#index'
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
- root 'welcome#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -56,4 +55,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
