bc79c729198f8feaf770438c11500a135ef92391
pcolusso <paulcolusso@gmail.com>
2015-01-29 07:28:45 +0000

    started nav work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  public def nav_link_to(name, controller)
+    #create <li class="active"><%= link_to 'Home', controller:  'welcome'%></li>
+  end
 end
