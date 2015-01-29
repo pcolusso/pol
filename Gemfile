ddcf8e44b0b52579b82fc9094ab4c68928d8fa2f
Paul Colusso <paulcolusso@gmail.com>
2015-01-29 21:31:03 +1100

    Added user model

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem "rails_12factor"
+gem 'bcrypt', '3.1.7'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
---
+++ test/models/user_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+	def setup
+		@user = User.new(name: "Test User", email: "test@test.com", level: 1, password: "blahblah")
+	end
+
+	test "should be vaild" do
+		assert @user.valid?
+	end
+
+	test "name should be present" do
+		@user.name = '    '
+		assert_not @user.valid?
+	end
+
+	test "email should be present" do
+		@user.email = '   '
+		assert_not @user.valid?
+	end
+
+	test "name should not be too long" do
+		@user.name = 'a' * 51
+		assert_not @user.valid?
+	end
+
+	test "email too long?" do
+		@user.email = 'a' * 2000 + "@example.com"
+		assert_not @user.valid?
+	end
+
+	test "email addresses should be unique" do
+    	duplicate_user = @user.dup
+    	duplicate_user.email = @user.email.upcase
+    	@user.save
+    	assert_not duplicate_user.valid?
+  	end
+
+  	test "password should have min length" do
+  		@user.password = 'w'
+  		assert_not @user.valid?
+  	end
+end
