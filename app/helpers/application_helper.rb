6a46223d2677d38218cf691714c221c751374594
Paul Colusso <paulcolusso@gmail.com>
2015-01-29 19:50:22 +1100

    Added nav helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	public def nav_link_to(link:, target_controller:)
+		#<li class="active"><%= link_to 'Home', controller:  'welcome'%></li>
+		link = link_to(link, controller: target_controller)
+		if (target_controller == controller.controller_name)
+			return content_tag(:li, link, class: "active")
+		else
+			return content_tag(:li, link)
+		end
+	end
 end
